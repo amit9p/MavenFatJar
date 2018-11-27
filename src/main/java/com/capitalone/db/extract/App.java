9ecc2407a475a64b681734f39bea634f9a9cbe56
AmitPrasad <amit_prasad@live.com>
2018-11-28 01:06:00 +0530

    maven fat jar

---
+++ src/main/java/com/capitalone/db/extract/App.java
@@ -0,0 +1,13 @@
+package com.capitalone.db.extract;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
